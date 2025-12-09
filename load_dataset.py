30b83d3eb4b67a1fab9156bcdc8ed2b786b7bb81
Takuma Kojima <j4320hfi@sit.ac.jp>
2025-12-09 15:36:31 +0900

    initial import

---
+++ load_dataset.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import torch
+from torchvision import datasets
+import torchvision.transforms.v2 as transforms
+
+ds_train = datasets.FashionMNIST(
+    root='data',
+    train=True,
+    download=True
+)
+
+print(f'dataset size: {len(ds_train)}')
+
+image, target = ds_train[0]
+
+print(type(image))
+print(target)
+
+plt.imshow(image, cmap='gray_r', vmin=0, vmax=255)
+plt.title(target)
+plt.show()
+
+image = transforms.functional.to_image(image)
+image = transforms.functional.to_dtype(image, scale=True)
+print(image.shape, image.dtype)
+print(image.min(), image.max())
